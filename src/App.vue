9d7b5c2c3db9dd02da849ac2a32c05105bd4f922
Nguyen Van Dai <nguyenvandaibe@gmail.com>
2021-08-27 22:50:29 +0700

    Thêm trang.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,49 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div id="nav">
+            <router-link to="/" class="p-1">Home</router-link>
+            <router-link to="/about" class="p-1">About</router-link>
+            <template v-if="!authenticated">
+                <router-link to="/dang-nhap" class="p-1">Đăng nhập</router-link>
+            </template>
+            <template v-else>
+                <router-link to="/tai-khoan">{{ user.name }}</router-link>
+                <router-link to="#" class="p-1">Đăng xuất</router-link>
+            </template>
+        </div>
+        <router-view />
     </div>
-    <router-view/>
-  </div>
 </template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+    computed: {
+        ...mapState({
+            authenticated: "auth/authenticated",
+            user: "auth/user",
+        }),
+    },
+};
+</script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+}
+#nav {
+    padding: 30px;
+    text-align: center;
+    a {
+        font-weight: bold;
+        color: #2c3e50;
+        text-decoration-line: none;
+        &.router-link-exact-active {
+            color: #42b983;
+        }
     }
-  }
 }
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-sm-4">
+            <div class="card shadow">
+                <div class="card-body">
+                    <form @submit.prevent="submit">
+                        <div class="mb-3">
+                            <label for="username">Tên đăng nhập</label>
+                            <input
+                                type="text"
+                                class="form-control"
+                                v-model="form.username"
+                            />
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="password">Mật khẩu</label>
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                                class="form-control"
+                                v-model="form.password"
+                            />
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">
+                            Đăng nhập
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            form: {
+                username: "",
+                password: "",
+            },
+        };
+    },
+};
+</script>
